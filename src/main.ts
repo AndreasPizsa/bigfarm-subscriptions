39764a8498e8e9ab7846b3d686a8d94fe4ecd1e7
ibabkin <ibabkin@goodgamestudios.com>
2019-04-23 13:37:34 +0200

    Introduce TS

---
+++ src/main.ts
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+
+import './assets/scss/main.scss'
+import BigFarm from './components/big-farm.vue'
+import StatusChangedModal from './components/status-changed.vue'
+import * as moment from "moment";
+
+Vue.config.productionTip = false;
+Vue.use(require('vue-moment'), {moment});
+
+const NotFound = {template: '<p>Page not found</p>'};
+
+if (process.env.VUE_APP_ANALYTICS_KEY) {
+    window.analytics.load(process.env.VUE_APP_ANALYTICS_KEY);
+    window.analytics.page();
+}
+
+const routes = {
+    '/': BigFarm,
+    '/subscriptionStatusChange': StatusChangedModal
+} as any;
+
+new Vue({
+    el: '#app',
+    data: {
+        currentRoute: window.location.pathname
+    },
+    computed: {
+        ViewComponent() {
+            return (routes[this.currentRoute] || NotFound) as any
+        }
+    },
+    render(h) {
+        return h((this as any).ViewComponent as any)
+    }
+}); //.$mount('#app')
